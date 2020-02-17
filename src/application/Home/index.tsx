7f1da476e7d0511a20cce470d63c32089ab47ee5
Weikai Wang <wc563388548@gmail.com>
2020-02-17 16:43:56 +0900

    set up router for home page and create styled component of Tab slider

--- src/application/Home/index.tsx
+++ src/application/Home/index.tsx
@@ -1,9 +1,38 @@
 // src/appliction/Home/index.tsx
 import React from 'react';
+import { renderRoutes } from "react-router-config";
+import { NavLink } from 'react-router-dom'; // 利用 NavLink 组件进行路由跳转
+import { Top, Tab, TabItem } from './HomeStyledComponents';
 
-const Home = () => {
+const Home = (props : any) => {
+  const { route } = props;
+
   return (
-    <div>Home</div>
+    <div>
+      <Top>
+        <span className="iconfont menu">&#xe630;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe7b3;</span>
+      </Top>
+      <Tab>
+        <NavLink to="/recommend" activeClassName="selected">
+          <TabItem>
+            <span > 推荐 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/singers" activeClassName="selected">
+          <TabItem>
+            <span > 歌手 </span>
+          </TabItem>
+        </NavLink>
+        <NavLink to="/rank" activeClassName="selected">
+          <TabItem>
+            <span > 排行榜 </span>
+          </TabItem>
+        </NavLink>
+      </Tab>
+      { renderRoutes (route.routes) }
+    </div>
   )
 }
 
