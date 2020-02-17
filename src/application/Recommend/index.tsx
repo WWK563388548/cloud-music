0a2ccff5241f12244d4cfad61b0b09b46047e420
Weikai Wang <wc563388548@gmail.com>
2020-02-17 20:03:24 +0900

    According to swiper library, creating a slider component

--- src/application/Recommend/index.tsx
+++ src/application/Recommend/index.tsx
@@ -1,9 +1,16 @@
 // src/appliction/Recommend/index.tsx
 import React from 'react';
+import Slider from '../../components/slider';
 
 const Recommend = () => {
+
+  // mock data
+  const bannerList = [1,2,3,4].map (item => {
+    return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
+  });
+
   return (
-    <div>Recommend</div>
+    <Slider bannerList={bannerList}></Slider>
   )
 }
 
---
+++ src/components/slider/index.tsx
@@ -0,0 +1,55 @@
+//components/slider/index.js
+import React, { useEffect, useState } from 'react';
+import { SliderContainer } from './SliderStyledComponents';
+import "swiper/css/swiper.css";
+import Swiper from "swiper";
+
+type SliderType = {
+    imageUrl: string
+}
+
+interface SliderProps {
+    bannerList: SliderType[]
+}
+
+const Slider = ({ bannerList }: SliderProps) => {
+  const [sliderSwiper, setSliderSwiper] = useState<null | object>(null);
+
+  useEffect (() => {
+    if (bannerList.length && !sliderSwiper){
+        let sliderSwiper = new Swiper(".slider-container", {
+          loop: true,
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
+          pagination: {el:'.swiper-pagination'},
+        });
+        setSliderSwiper(sliderSwiper);
+    }
+  }, [bannerList.length, sliderSwiper]);
+  
+  return (
+    <SliderContainer>
+      <div className="before" />
+      <div className="slider-container">
+        <div className="swiper-wrapper">
+          {
+            bannerList.map (slider => {
+              return (
+                <div className="swiper-slide" key={slider.imageUrl}>
+                  <div className="slider-nav">
+                    <img src={slider.imageUrl} width="100%" height="100%" alt="推荐" />
+                  </div>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className="swiper-pagination"></div>
+      </div> 
+    </SliderContainer>
+  );
+}
+
+export default React.memo (Slider);
