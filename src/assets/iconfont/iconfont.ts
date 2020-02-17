55d45f75192c7fa7aa6980d7f80164eec0091051
Weikai Wang <wc563388548@gmail.com>
2020-02-17 15:34:53 +0900

    set up icon library

---
+++ src/assets/iconfont/iconfont.ts
@@ -0,0 +1,21 @@
+import {createGlobalStyle} from'styled-components';
+
+export const IconStyle = createGlobalStyle`
+  @font-face {
+    font-family: "iconfont";
+    src: url('iconfont.eot?t=1581920352109'); /* IE9 */
+    src: url('iconfont.eot?t=1581920352109#iefix') format('embedded-opentype'), /* IE6-IE8 */
+    url('data:application/x-font-woff2;charset=utf-8;base64,d09GMgABAAAAAALAAAsAAAAABmAAAAJyAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHEIGVgCCcApsgQwBNgIkAwgLBgAEIAWEbQcwG7IFyC6wbdiTQGyMjkYbfHa9FmcOMEQQ7febvXfvi+JJNblk0UQm0SiJBLFROqFo6PB/a2129sAbjTQ03727iHpSWfmz4rMzJw0tySuJSuR1iEQ8i+ZLgFNrnRwAt/USDTEIs+aJvJXUops+cT/1r4AGku98N5eIoKjkSMCBTQcYVTSyTWjGG8YucAnHCTSM6iSdjc8uoFqZNS4QT46lodqcV5ZZoiZUapZm8YZKtfSCb/Eafj9+rUQtSVmwSi4fx0w0+Dl2irxb2fXgE8hHQKtLFFiGZOK2NnpOFowua0QzJqF9FQafU6n4FrFXo2B/nZXfAsNQdE9iV3Kr+QnkuHsUsjXqFlJfo3d/SMsYXLrWePsHqRX8SO7Za7ginRxjIbesvdXld9161dwp8PEJFnbpcWJvVSqVJI97rVRcxWu4WMxZLZbQIOtnxQW+EQj+wou3u73/tWYGHyE7oSHrZ6e2Z1vwh8Q9m7Iutcwpq+JaezI6amxCQwP/sLPP41jd4O0kQ83gTdmrRlIUaqZJmbaMUtMaKmoO0bBkYnVTP0YWuY5FRwChxxWSjncUetyRMu0BpSHvqOgJGQ330bRl02w49SMMNA4m0cvEMaKQOlZeHxXOgSV8jWWlIWMJWOoESE7JFhMTEAKbY0ZasPKcU0JZFJBxcBr4fkRiFrlgcMXmPO5WVVr3JsWIAlTvFAM0HDARujLCYYiEqN+Zqy99fg6wCD4Naymr8S4BTMrpnchRZHuQJlRhr7J7eSVVYMnjOIqgmEiAGAeDgM8XIeL6QS5g4BT2iHCsm2q3o31Vyvqa4AMKWRWWPoXdr1B5Bo0ZNkIAAAA=') format('woff2'),
+    url('iconfont.woff?t=1581920352109') format('woff'),
+    url('iconfont.ttf?t=1581920352109') format('truetype'), /* chrome, firefox, opera, Safari, Android, iOS 4.2+ */
+    url('iconfont.svg?t=1581920352109#iconfont') format('svg'); /* iOS 4.1- */
+  }
+
+  .iconfont {
+    font-family: "iconfont" !important;
+    font-size: 16px;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
