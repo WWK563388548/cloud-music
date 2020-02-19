64c57a621f8a733a8538fc8e0e6630c7bc96fc0e
Weikai Wang <wc563388548@gmail.com>
2020-02-19 11:56:54 +0900

    creating reducer of Recommend page

---
+++ src/store/Recommend/actionCreators.ts
@@ -0,0 +1,41 @@
+import { axiosInstance } from "../../lib/config";
+import { Dispatch } from 'redux';
+import * as actionTypes from './constants';
+import { fromJS } from 'immutable'; // 将 JS 对象转换成 immutable 对象
+import { RecommendListItem, BannerListItem, ReponseType } from './types';
+
+export const changeBannerList = (data: BannerListItem[] | void) => ({
+  type: actionTypes.CHANGE_BANNER,
+  data: fromJS (data)
+});
+
+export const changeRecommendList = (data: RecommendListItem[] | void) => ({
+  type: actionTypes.CHANGE_RECOMMEND_LIST,
+  data: fromJS (data)
+});
+
+export const getBannerList = () => {
+  return (dispatch: Dispatch) => {
+    axiosInstance
+    .get('/banner')
+    .then (({ banners }: ReponseType ) => {
+      dispatch (changeBannerList (banners));
+    }).catch ((e) => {
+      alert(e);
+      console.log ("轮播图数据传输错误");
+    }) 
+  }
+};
+
+export const getRecommendList = () => {
+  return (dispatch: Dispatch) => {
+    axiosInstance
+    .get('/personalized')
+    .then (({ result }: ReponseType ) => {
+      dispatch (changeRecommendList (result));
+    }).catch ((e) => {
+      alert(e);
+      console.log ("推荐歌单数据传输错误");
+    });
+  }
+};
---
+++ src/store/Recommend/types.ts
@@ -0,0 +1,46 @@
+import { AxiosResponse } from 'axios';
+
+export const CHANGE_BANNER = 'CHANGE_BANNER';
+export const CHANGE_RECOMMEND_LIST = 'RECOMMEND_LIST';
+
+export interface BannerListItem {
+  imageUrl: string
+  targetId: number
+  adid: null | number
+  targetType: number
+  titleColor: string
+  typeTitle: string
+  url: null | string
+  encodeId: string
+}
+  
+export interface RecommendListItem {
+  id: number
+  type: number
+  name: string
+  copywriter: string
+  picUrl: string
+  canDislike: boolean
+  trackNumberUpdateTime: number
+  playCount: number
+  trackCount: number
+  highQuality: boolean
+  alg: string
+}
+
+export interface ReponseType extends AxiosResponse {
+  banners?: BannerListItem[]
+  result?: RecommendListItem[]
+}
+
+
+export interface ChangeBannerList {
+  type: typeof CHANGE_BANNER
+  data: BannerListItem[]
+}
+export interface ChangeRecommendList {
+  type: typeof CHANGE_RECOMMEND_LIST
+  data: RecommendListItem[]
+}
+
+export type RecommendActionType = ChangeBannerList | ChangeRecommendList
