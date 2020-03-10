c4927cae53a6fe24d9eee32fcc724f6cabf8b827
Weikai Wang <wc563388548@gmail.com>
2020-03-10 18:14:29 +0900

    Creating screen of Singers with mock data

--- src/application/Singers/index.tsx
+++ src/application/Singers/index.tsx
@@ -2,17 +2,14 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import HorizontalList from '../../components/horizontalList';
+import Scroll from '../../components/scroll';
 import { categoryTypes, alphaTypes } from '../../lib/singersMockData';
-
-const NavContainer  = styled.div`
-  box-sizing: border-box;
-  position: fixed;
-  top: 95px;
-  width: 100%;
-  padding: 5px;
-  overflow: hidden;
-  background-color: #D3D3D3;
-`;
+import {
+  NavContainer,
+  ListContainer, 
+  List,
+  ListItem,
+} from './singersStyledComponents';
 
 const Singers = () => {
   const [category, setCategory] = useState<string>('');
@@ -25,6 +22,36 @@ const Singers = () => {
   const handleUpdateCatetory = (val: string) => {
     setCategory (val);
   }
+
+  // mock data
+  const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
+    return {
+      tempId: item,
+      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
+      name: "Someone",
+      accountId: 111,
+    }
+  }); 
+
+  const renderSingerList = () => {
+    return (
+      <List>
+        {
+          singerList.map ((item, index) => {
+            return (
+              <ListItem key={item.tempId + " " + index}>
+                <div className="img_wrapper">
+                  <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music"/>
+                </div>
+                <span className="name">{item.name}</span>
+              </ListItem>
+            )
+          })
+        }
+      </List>
+    )
+  };
+
   return (
     <NavContainer>
       <HorizontalList 
@@ -39,6 +66,11 @@ const Singers = () => {
         handleClick={val => handleUpdateAlpha(val)} 
         oldVal={alpha}
       />
+      <ListContainer>
+        <Scroll>
+          { renderSingerList () }
+        </Scroll>
+      </ListContainer>
     </NavContainer>
   )
 }
---
+++ src/application/Singers/singersStyledComponents.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const NavContainer  = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  top: 95px;
+  width: 100%;
+  padding: 5px;
+  overflow: hidden;
+  background-color: #D3D3D3;
+`;
+
+export const ListContainer = styled.div`
+  position: fixed;
+  top: 160px;
+  left: 0;
+  bottom: 0;
+  overflow: hidden;
+  width: 100%;
+`;
+
+export const List = styled.div`
+  display: flex;
+  margin: auto;
+  flex-direction: column;
+  overflow: hidden;
+  .title {
+    margin:10px 0 10px 10px;
+    color: #2E3030;
+    font-size: 12px;
+  }
+`;
+
+export const ListItem = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  margin: 0 5px;
+  padding: 5px 0;
+  align-items: center;
+  border-bottom: 1px solid #e4e4e4;
+  .img_wrapper {
+    margin-right: 20px;
+    img {
+      border-radius: 3px;
+      width: 50px;
+      height: 50px;
+    }
+  }
+  .name {
+    font-size: 14px;
+    color: #2E3030;
+    font-weight: 500;
+  }
+`;
