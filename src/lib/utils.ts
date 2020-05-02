9f3948ab18a9ffa6b93f11eb5a769adef3dd7d9d
Weikai Wang <wc563388548@gmail.com>
2020-05-02 22:49:40 +0900

    creating debounce func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,3 +11,19 @@ export const getCount = (count: number): string | boolean | number => {
       return Math.floor (count / 10000000)/ 10 + "亿";
     }
   }
+
+// debounce func
+export const debounce = (func: any, delay: number) => {
+  let timer: any;
+  // @ts-ignore
+  return function (...args) {
+    if (timer) {
+      clearTimeout (timer);
+    }
+    timer = setTimeout (() => {
+      // @ts-ignore
+      func.apply (this, args);
+      clearTimeout (timer);
+    }, delay);
+  }
+}
