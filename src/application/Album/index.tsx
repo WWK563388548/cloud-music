fc5d6b5a24d038195b6389ae75076cb151d99521
Weikai Wang <wc563388548@gmail.com>
2020-05-06 19:46:00 +0900

    Adding comments and header component of album

--- src/application/Album/index.tsx
+++ src/application/Album/index.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import styled from'styled-components';
 import { CSSTransition } from 'react-transition-group';
+import Header from '../../components/header';
 
 
 const Container = styled.div`
@@ -32,6 +33,13 @@ const Container = styled.div`
 const Album = (props: any) => {
   const [showStatus, setShowStatus] = useState(true);
 
+  const handleBack = () => {
+    // 不直接在 handleBack 中跳转路由
+    // 先让页面切出动画执行一次，然后在动画执行完的瞬间跳转路由
+    // 因为如果路由变化, 当前的组件会被立即卸载, 相关的动画当然也就不复存在了
+    setShowStatus (false);
+  };
+
   return (
     <CSSTransition
       in={showStatus}
@@ -39,9 +47,10 @@ const Album = (props: any) => {
       classNames="fly"
       appear={true}
       unmountOnExit
-      onExited={props.history.goBack}
+      onExited={props.history.goBack} // 先让页面切出动画执行一次，然后在动画执行完的瞬间跳转路由
     >
       <Container>
+        <Header title={'返回'} handleClick={handleBack} />
       </Container>
     </CSSTransition>
   )
---
+++ src/components/header/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from'styled-components';
+
+const HeaderContainer = styled.div`
+  position: fixed;
+  padding: 5px 10px;
+  padding-top: 0;
+  height: 40px;
+  width: 100%;
+  z-index: 100;
+  display: flex;
+  line-height: 40px;
+  color: #F1F1F1;
+  .back {
+    margin-right: 5px;
+    font-size: 20px;
+    width: 20px;
+  }
+  > h1 {
+    font-size: 16px;
+    font-weight: 700;
+  }
+`;
+
+interface Props {
+  handleClick: () => void
+  title: string
+}
+// 处理函数组件拿不到 ref 的问题，所以用 forwardRef
+const Header = React.forwardRef ((props: Props, ref: any) => {
+  const { handleClick, title} = props;
+  // TODO: Need to Update iconFont
+  return (
+    <HeaderContainer ref={ref}>
+      <i className="iconfont back"  onClick={handleClick}>&#xe655;</i>
+      <h1>{title}</h1>
+    </HeaderContainer>
+  )
+})
+
+Header.defaultProps = {
+  handleClick: () => {},
+  title: "标题",
+};
+
+export default React.memo (Header);
