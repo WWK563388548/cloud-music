a1deba86593120be130501e3c73e8855f8e78de5
Weikai Wang <wc563388548@gmail.com>
2020-05-06 20:11:37 +0900

    Creating Mock data for album

--- src/lib/singersMockData.tsx
+++ src/lib/singersMockData.tsx
@@ -195,3 +195,86 @@ export const RankTypes = {
   "22": "云音乐ACG音乐榜",
   "23": "江小白YOLO云音乐说唱榜"
 }; 
+
+// mock 数据
+export const currentAlbum = {
+  creator: {
+    avatarUrl: "http://p1.music.126.net/O9zV6jeawR43pfiK2JaVSw==/109951164232128905.jpg",
+    nickname: "浪里推舟"
+  },
+  coverImgUrl: "http://p2.music.126.net/ecpXnH13-0QWpWQmqlR0gw==/109951164354856816.jpg",
+  subscribedCount: 2010711,
+  name: "听完就睡，耳机是天黑以后柔软的梦境",
+  tracks:[
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+    {
+      name: "我真的受伤了",
+      ar: [{name: "张学友"}, {name: "周华健"}],
+      al: {
+        name: "学友 热"
+      }
+    },
+  ]
+}
