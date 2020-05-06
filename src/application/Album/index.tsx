275bd25760b197618de4720caca0946b121a4306
Weikai Wang <wc563388548@gmail.com>
2020-05-06 11:53:43 +0900

    Adding animation for route change

--- src/application/Album/index.tsx
+++ src/application/Album/index.tsx
@@ -1,6 +1,7 @@
 // src/application/Album/index.js
-import React from 'react';
+import React, { useState } from 'react';
 import styled from'styled-components';
+import { CSSTransition } from 'react-transition-group';
 
 
 const Container = styled.div`
@@ -9,16 +10,41 @@ const Container = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  z-index: 100;
+  z-index: 1000;
   background: #fff;
+  transform-origin: right bottom;
+  &.fly-enter, &.fly-appear{
+    transform: rotateZ(30deg) translate3d(100%, 0, 0);
+  }
+  &.fly-enter-active, &.fly-appear-active{
+    transition: transform .3s;
+    transform: rotateZ(0deg) translate3d(0, 0, 0);
+  }
+  &.fly-exit{
+    transform: rotateZ(0deg) translate3d(0, 0, 0);
+  }
+  &.fly-exit-active{
+    transition: transform .3s;
+    transform: rotateZ(30deg) translate3d(100%, 0, 0);
+  }
 `;
 
-function Album () {
+const Album = (props: any) => {
+  const [showStatus, setShowStatus] = useState(true);
+
   return (
-    <Container>
-      Album
-    </Container>
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
+      unmountOnExit
+      onExited={props.history.goBack}
+    >
+      <Container>
+      </Container>
+    </CSSTransition>
   )
 }
 
-export default Album;
+export default React.memo (Album);
