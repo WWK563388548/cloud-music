7f34b47785c018137ce6569228161d27a185719d
Weikai Wang <wc563388548@gmail.com>
2020-06-15 16:00:47 +0900

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
-export const baseUrl = 'http://localhost:4000';
+export const baseUrl = 'http://localhost:3000';
 
 // axios 的实例及拦截器配置
 const axiosInstance = axios.create ({
